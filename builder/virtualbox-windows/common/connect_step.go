6d346b22912299f52aa0e8e79a84a38392cbec6b
Matt Fellows <matt.fellows@onegeek.com.au>
2014-12-07 11:45:46 +1100

    Removed SSH references from the connection step

--- builder/virtualbox-windows/common/connect_step.go
+++ builder/virtualbox-windows/common/connect_step.go
@@ -3,8 +3,6 @@ package common
 import (
 	"fmt"
 	"github.com/mitchellh/multistep"
-	vboxcommon "github.com/mitchellh/packer/builder/virtualbox/common"
-	"github.com/mitchellh/packer/common"
 	wincommon "github.com/packer-community/packer-windows-plugins/common"
 	"log"
 )
@@ -22,20 +20,11 @@ func WinRMAddressFunc(config wincommon.WinRMConfig) func(state multistep.StateBa
 }
 
 // Creates a generic SSH or WinRM connect step from a VMWare builder config
-func NewConnectStep(communicatorType string, driver vboxcommon.Driver, sshConfig vboxcommon.SSHConfig, winrmConfig wincommon.WinRMConfig) multistep.Step {
-	//if communicatorType == packer.WinRMCommunicatorType {
-	if communicatorType == "winrm" {
-		return &wincommon.StepConnectWinRM{
-			WinRMAddress:     WinRMAddressFunc(winrmConfig),
-			WinRMUser:        winrmConfig.WinRMUser,
-			WinRMPassword:    winrmConfig.WinRMPassword,
-			WinRMWaitTimeout: winrmConfig.WinRMWaitTimeout,
-		}
-	} else {
-		return &common.StepConnectSSH{
-			SSHAddress:     vboxcommon.SSHAddress,
-			SSHConfig:      vboxcommon.SSHConfigFunc(sshConfig),
-			SSHWaitTimeout: sshConfig.SSHWaitTimeout,
-		}
+func NewConnectStep(winrmConfig wincommon.WinRMConfig) multistep.Step {
+	return &wincommon.StepConnectWinRM{
+		WinRMAddress:     WinRMAddressFunc(winrmConfig),
+		WinRMUser:        winrmConfig.WinRMUser,
+		WinRMPassword:    winrmConfig.WinRMPassword,
+		WinRMWaitTimeout: winrmConfig.WinRMWaitTimeout,
 	}
 }
