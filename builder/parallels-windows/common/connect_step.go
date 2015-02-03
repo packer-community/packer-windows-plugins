add2abcc16209226ed9c85970eecab09e0b2b737
Matt Fellows <matt.fellows@onegeek.com.au>
2015-02-03 21:05:09 +1100

    Detecting Parallels guest virtual IP address by querying the Driver.

--- builder/parallels-windows/common/connect_step.go
+++ builder/parallels-windows/common/connect_step.go
@@ -5,24 +5,34 @@ import (
 	"log"
 
 	"github.com/mitchellh/multistep"
+	parallelscommon "github.com/mitchellh/packer/builder/parallels/common"
 	wincommon "github.com/packer-community/packer-windows-plugins/common"
 )
 
 func WinRMAddressFunc(config wincommon.WinRMConfig) func(state multistep.StateBag) (string, error) {
-	if config.WinRMHost == "" {
-		log.Printf("No WinRM Host provided, using default host 127.0.0.1")
-		config.WinRMHost = "127.0.0.1"
-	}
-
-	log.Printf("Have address from config: %s:%d", config.WinRMHost, config.WinRMPort)
 
 	return func(state multistep.StateBag) (string, error) {
+		log.Printf("Determining WinRM remote IP address...")
+		vmName := state.Get("vmName").(string)
+		driver := state.Get("driver").(parallelscommon.Driver)
+
+		mac, err := driver.Mac(vmName)
+		if err != nil {
+			return "", err
+		}
+
+		ip, err := driver.IpAddress(mac)
+		if err != nil {
+			return "", err
+		}
+
 		winrmPort := config.WinRMPort
 		if forwardedPort, ok := state.GetOk("winrmHostPort"); ok {
 			winrmPort = forwardedPort.(uint)
 		}
 
-		return fmt.Sprintf("%s:%d", config.WinRMHost, winrmPort), nil
+		log.Printf("Detected WinRM address to be: %s:%d", ip, winrmPort)
+		return fmt.Sprintf("%s:%d", ip, winrmPort), nil
 	}
 }
 
